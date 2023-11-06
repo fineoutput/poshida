f21582dac3fcd691cbf997ca90c16468042a6d71
Pawan <office.fineoutput@gmail.com>
2023-11-06 16:13:15 +0530

    slider

--- application/views/frontend/product_details.php
+++ application/views/frontend/product_details.php
@@ -1,8 +1,20 @@
 <!-- Product detail contant start -->
+<link rel="stylesheet" href="https://www.insightindia.com/mcss/icon-font.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.2/css/bootstrap.min.css"> -->
 <style>
   html {
     scroll-behavior: smooth;
   }
+  .breadcrumb {
+    background-color: transparent !important;
+    display: unset;
+    padding: 0;
+    margin: 0;
+    float: right;
+}
+
 
   .product-details-btn ul {
     display: flex;
@@ -103,6 +115,102 @@
     visibility: visible;
     opacity: 1;
   }
+
+  .slick-slider .slick-prev, .slick-slider .slick-next {
+  z-index: 100;
+  font-size: 2.5em;
+  height: 46px;
+  width: 40px;
+  margin-top: -20px;
+  color: #B7B7B7;
+  position: absolute;
+  top: 50%;
+  text-align: center;
+  color: #000;
+  opacity: .3;
+  transition: opacity .25s;
+  cursor: pointer;
+}
+.slick-slider .slick-prev:hover, .slick-slider .slick-next:hover {
+  opacity: .65;
+}
+.slick-slider .slick-prev {
+  left: 0;
+}
+.slick-slider .slick-next {
+  right: 0;
+}
+
+#detail .product-images {
+  width: 100%;
+  margin: 0 auto;
+  border:1px solid #eee;
+}
+#detail .product-images li, #detail .product-images figure, #detail .product-images a, #detail .product-images img {
+  display: block;
+  outline: none;
+  border: none;
+}
+#detail .product-images .main-img-slider figure {
+  margin: 0 auto;
+  padding: 0 2em;
+}
+#detail .product-images .main-img-slider figure a {
+  cursor: pointer;
+  cursor: -webkit-zoom-in;
+  cursor: -moz-zoom-in;
+  cursor: zoom-in;
+}
+#detail .product-images .main-img-slider figure a img {
+  width: 100%;
+  max-width: 400px;
+  margin: 0 auto;
+}
+#detail .product-images .thumb-nav {
+  margin: 0 auto;
+  padding:20px 10px;
+  max-width: 600px;
+}
+#detail .product-images .thumb-nav.slick-slider .slick-prev, #detail .product-images .thumb-nav.slick-slider .slick-next {
+  font-size: 1.2em;
+  height: 20px;
+  width: 26px;
+  margin-top: -10px;
+}
+#detail .product-images .thumb-nav.slick-slider .slick-prev {
+  margin-left: -30px;
+}
+#detail .product-images .thumb-nav.slick-slider .slick-next {
+  margin-right: -30px;
+}
+#detail .product-images .thumb-nav li {
+  display: block;
+  margin: 0 auto;
+  cursor: pointer;
+}
+#detail .product-images .thumb-nav li img {
+  display: block;
+  width: 100%;
+  max-width: 75px;
+  margin: 0 auto;
+  border: 2px solid transparent;
+  -webkit-transition: border-color .25s;
+  -ms-transition: border-color .25s;
+  -moz-transition: border-color .25s;
+  transition: border-color .25s;
+}
+#detail .product-images .thumb-nav li:hover, #detail .product-images .thumb-nav li:focus {
+  border-color: #999;
+}
+#detail .product-images .thumb-nav li.slick-current img {
+  border-color: #d12f81;
+}
+
+
+.slick-slider .slick-prev, .slick-slider .slick-next {
+    background: white;
+    padding: 7px 0px;
+}
 </style>
 
 
@@ -168,10 +276,10 @@
             $image1 = $type_data[0]->image;
           } ?>
 
-          <div class="sk"></div>
-          <div class="sj"></div>
-          <div class="align-center mb-md-30">
-            <!-- <ul id="glasscase" class="gc-start">
+          <!-- <div class="sk"></div>
+          <div class="sj"></div> -->
+          <!-- <div class="align-center mb-md-30">
+            <ul id="glasscase" class="gc-start">
               <li><img src="<?= base_url() . $type_data[0]->image ?>" alt=" "></li>
               <? if (!empty($type_data[0]->image2)) { ?><li><img src="<?= base_url() .  $type_data[0]->image2 ?>" alt=" "></li> <? }
                                                                                                                               if (!empty($type_data[0]->image3)) { ?>
@@ -186,6 +294,39 @@
                 <li><img src="<?= base_url() .  $type_data[0]->video ?>"></li> <? } 
                 ?>
             </ul> -->
+
+            
+                        <section id="detail">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-12 mx-auto">
+               
+                      <div class="product-images demo-gallery">
+                        
+                        <div class="main-img-slider">
+                           <a  data-fancybox="gallery" href="https://endtest-videos.s3-us-west-2.amazonaws.com/documentation/endtest_data_driven_testing_csv.mp4"> <img  src="https://www.poshida.in/assets/uploads/blog/blog20231105135118.jpg" class="img-fluid gc-zoom"></a>
+                           <a data-fancybox="gallery" href="https://endtest-videos.s3-us-west-2.amazonaws.com/documentation/endtest_data_driven_testing_csv.mp4"> <img src="https://www.poshida.in/assets/uploads/blog/blog20231105135118.jpg" class="img-fluid"></a>
+                           <a data-fancybox="gallery" href="https://endtest-videos.s3-us-west-2.amazonaws.com/documentation/endtest_data_driven_testing_csv.mp4"> <img src="https://www.poshida.in/assets/uploads/blog/blog20231105135118.jpg" class="img-fluid"></a>
+                           <a data-fancybox="gallery" href="https://endtest-videos.s3-us-west-2.amazonaws.com/documentation/endtest_data_driven_testing_csv.mp4"> <img src="https://www.poshida.in/assets/uploads/blog/blog20231105135118.jpg" class="img-fluid"></a>
+                        </div>
+            
+                    
+                      <ul class="thumb-nav">
+                        <li><img src="https://www.poshida.in/assets/uploads/blog/blog20231105135118.jpg"></li>
+                        <li><img src="https://www.poshida.in/assets/uploads/blog/blog20231105135118.jpg"></li>
+                        <li><img src="https://www.poshida.in/assets/uploads/blog/blog20231105135118.jpg"></li>
+                      
+                      </ul>
+              
+                    </div>
+                  
+                    
+                  </div>
+                </div>
+              </div>
+              
+              
+            </section> 
           </div>
         </div>
         <div class="col-lg-7 col-md-6 col-12">
@@ -806,6 +947,12 @@
     </div>
   </section>
 <? } ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/3.0.1/jquery-migrate.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
 <script>
  
   function scrollMe (){
@@ -814,6 +961,52 @@
       },
       'slow');
   };
+  
+</script>
+<script>
+ /*--------------*/
+
+
+
+// Main/Product image slider for product page
+$('#detail .main-img-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: true,
+  arrows: true,
+  fade:true,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  speed: 300,
+  lazyLoad: 'ondemand',
+  asNavFor: '.thumb-nav',
+  prevArrow: '<div class="slick-prev"><i class="i-prev"></i><span class="sr-only sr-only-focusable">Previous</span></div>',
+  nextArrow: '<div class="slick-next"><i class="i-next"></i><span class="sr-only sr-only-focusable">Next</span></div>'
+});
+// Thumbnail/alternates slider for product page
+$('.thumb-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  infinite: true,
+  centerPadding: '0px',
+  asNavFor: '.main-img-slider',
+  dots: false,
+  centerMode: false,
+  draggable: true,
+  speed:200,
+  focusOnSelect: true,
+  prevArrow: '<div class="slick-prev"><i class="i-prev"></i><span class="sr-only sr-only-focusable">Previous</span></div>',
+  nextArrow: '<div class="slick-next"><i class="i-next"></i><span class="sr-only sr-only-focusable">Next</span></div>'  
+});
+
+
+//keeps thumbnails active when changing main image, via mouse/touch drag/swipe
+$('.main-img-slider').on('afterChange', function(event, slick, currentSlide, nextSlide){
+  //remove all active class
+  $('.thumb-nav .slick-slide').removeClass('slick-current');
+  //set active class for current slide
+  $('.thumb-nav .slick-slide:not(.slick-cloned)').eq(currentSlide).addClass('slick-current');  
+});
 </script>
 
 
