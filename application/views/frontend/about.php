077bf30f806471b48c46dacf2362826c93080887
Kunal <kunalsoni2833@gmail.com>
2024-02-13 17:20:02 +0530

    update

--- application/views/frontend/about.php
+++ application/views/frontend/about.php
@@ -28,39 +28,24 @@
     <div class="container mt-3">
       <div class="row">
         <div class="col-lg-12 text-center">
-          <span style="font-size:30px; margin-bottom:10px; "> <b> Who We Are </b></span>
+          <span style="font-size:30px; margin-bottom:10px; "> <b> Welcome to Poshida - Crafting Elegance Since 2018 </b></span>
           <hr>
         </div>
       </div>
-      <p>We proudly stand as one of India's foremost fashion retailers, offering an extensive
-        array of high-quality products that cater to diverse tastes. With a strong presence
-        both online and in our brick-and-mortar stores, we have firmly established ourselves
-        as an industry leader.</p>
-      <p>Founded in Jaipur, our journey began as a ready-to-wear brand that has since
-        become synonymous with versatile and empowering casual attire. Since our
-        inception in 2018, we have continuously evolved and expanded our product range,
-        endearing ourselves to a devoted customer base.</p>
-      <p>Throughout our growth, we have remained steadfast in our commitment to a core
-        principle: the customer always takes center stage in everything we undertake. Our
-        familial approach to working closely together has allowed us to excel across all
-        facets of fashion production, encompassing design, manufacturing, distribution, and
-        retail. This approach, underpinned by three key pillars—flexibility, digital integration,
-        and sustainability—has brought us closer than ever to our customers, delivering the
-        products they desire at affordable prices..</p>
-      <!-- <p style="color: black;"> <b>Established in Jaipur, this ready-to-wear brand has come to be synonymous with versatile yet powerful casual-wear.</b> </p>
-    <p style="color: black;"> <b>We have been constantly evolving and expanding the product line and ruling the hearts of loyal customers since 2019.</b> </p>
-    <p>Over the years, our size may have changed, but one underlying idea has stayed the same – the customer is at the centre of everything we do.</p>
-    <p>By working closely together as a family, WE focused on the key elements of fashion production –design, manufacture, distribution and retail– with three pillars -flexibility, digital integration and sustainability- we brought our customers
-      closer than ever to the products they wanted at affordable prices.</p> -->
-      <h4>Value beyond profit</h4>
-      <!-- <p style="color: black;"> <u>poshida brand is an Exclusive Collection for more meaningful buying.</u> </p> -->
-      <p>Our brand represents more than just fashion; it embodies an exclusive collection that
-        promotes meaningful consumption. Our dedicated workforce consistently prioritizes
-        the customer's needs and values, transcending mere profitability. We strive to create
-        value that extends beyond financial gain, emphasizing both people and the
-        environment in our decision-making processes. At the core of our business
-        philosophy, we believe in providing fashion that is not only stylish but also ethical
-        and sustainable—fashion that is truly "Right to Wear."
+      <p>In the vibrant tapestry of Indian fashion, <b> Poshida, Grace In Every Stitch </b> stands out as a beacon of style and tradition. Established in 2018 by the visionary <b> Usha Singh, </b> our brand has become synonymous with quality, affordability, and timeless fashion. From the heart of India, we bring you an exquisite range of Women's wear, Men's wear, Kids wear, and Accessories, ensuring that every family member can indulge in the joy of fashionable clothing.
+</p>
+      
+      <h4>A Seamless Blend of Tradition and Trend</h4>
+
+      <p><b> Poshida </b> takes pride in offering a diverse collection of clothing that seamlessly blends the richness of Indian traditions with contemporary fashion trends. Our commitment to quality is reflected in premium cotton fabrics, providing comfort without compromising style. From ethnic ensembles to chic casual wear, <b> Poshida </b> is your go-to destination for all things fashion.
+
+      </p>
+      <h4>We are empowering partners - B to B and B to C Excellence.</h4>
+
+      <p><b> Poshida </b> is not merely a fashion brand; it's a collaborative journey that extends its reach to both Business-to-Business (B to B) and Business-to-Consumer (B to C) landscapes, creating a comprehensive ecosystem within the world of fashion. For our BtoB partners, we offer more than just products – we provide opportunities for growth and prosperity. Our commitment to empowering resellers and retailers transcends transactions; it's a symbiotic relationship where success is mutual. Through seamless partnerships, we share the essence of <b> Poshida </b> with retailers, ensuring our carefully crafted garments become not just merchandise but a cornerstone of their success.
+      <br>
+      Simultaneously, our B to C commitment is to bring the vibrant spirit of <b> Poshida </b> directly to our customers. Whether you explore our collections online, discover the latest trends from the comfort of your home, or step into our physical stores to experience the tactile allure of our fabrics,<b> Poshida </b>ensures that every interaction is a celebration of individual style. Our dedication to quality and customization shines through in every piece, making the B to C experience not just a purchase but a journey into the heart of fashion. In both B to B and B to C realms, <b> Poshida </b> doesn't just provide clothing; it fosters a sense of community, where each garment carries the story of tradition, innovation, and the collective pursuit of elegance.
+
       </p>
       <div class="container">
         <div class="row text-center">
@@ -69,31 +54,25 @@
           </div> -->
           <div class="col-md-12 mt-2" style="border:10px solid #d65555;border-bottom:50px solid #d65555;">
             <h4>OUR BUSINESS MODEL</h4>
-            <p style="text-align: justify;">Design: We craft stylish clothing that reflects the latest trends and your
-              personal style.
-            </p>
-            <p style="text-align: justify;">Making Stuff: Our dedicated team ensures each item is meticulously crafted
-              for quality and durability.
+            <p style="text-align: justify;">Design: We craft stylish clothing that reflects the latest trends and your style.
 
             </p>
-            <p style="text-align: justify;">Sourcing: We responsibly acquire materials, considering their origin and
-              impact on the environment.
-            </p>
-            <p style="text-align: justify;">Quality Control: Every product undergoes rigorous checks to ensure it meets
-              our high standards.
-            </p>
-            <p style="text-align: justify;">Getting It to You: We efficiently distribute our products to our stores and
-              online platforms for your convenience
-            </p>
-            <p style="text-align: justify;">Sustainability: We're committed to eco-friendly practices, aiming to minimize
-              our environmental footprint.
+            <p style="text-align: justify;">Making Stuff: Our dedicated team ensures each item is meticulously crafted for quality and durability.
+
 
             </p>
-            <p style="text-align: justify;">Flexibility: We adapt swiftly to changes and embrace innovative ideas in our
-              business operations.
+            <p style="text-align: justify;">Sourcing: We responsibly acquire materials, considering their origin and impact on the environment.
             </p>
-            <p style="text-align: justify;">Digital Integration: We leverage technology to enhance efficiency and
-              customer experience in every aspect of our work.
+            <p style="text-align: justify;">Quality Control: Every product undergoes rigorous checks to ensure it meets our high standards.
+            </p>
+            <p style="text-align: justify;">Getting It to You: We efficiently distribute our products to our stores and online platforms for your convenience
+            </p>
+            <p style="text-align: justify;">Sustainability: We're committed to eco-friendly practices, aiming to minimize our environmental footprint.
+
+            </p>
+            <p style="text-align: justify;">Flexibility: We adapt swiftly to changes and embrace innovative ideas in our business operations.
+            </p>
+            <p style="text-align: justify;">Digital Integration: We leverage technology to enhance efficiency and customer experience in every aspect of our work.
             </p>
           </div>
         </div>
@@ -107,30 +86,15 @@
         <h4>Key Attributes:
         </h4>
         <!-- <p style="color: black;"> <u>poshida brand is an Exclusive Collection for more meaningful buying.</u> </p> -->
-        <p>ISO Certified: We're all about quality and excellence. Our ISO certification
-          shows that we're really serious about meeting international standards and
-          making fashion that lasts.
+        <p>ISO Certified: We're all about quality and excellence. Our ISO certification shows that we're serious about meeting international standards and making fashion that lasts.        
         </p>
-        <p>Factory 100% Compliance: Our factories are more than just places where
-          things are made. They follow all the rules really carefully to make sure our
-          workers are safe, our facilities are good, and we don't harm the environment.
-          We're all about doing things the right way.
+        <p>Factory 100% Compliance: Our factories are more than just places where things are made. They follow all the rules carefully to make sure our workers are safe, our facilities are good, and we don't harm the environment. We're all about doing things the right way.    
         </p>
-        <p>A Dynamic and Youthful Team: Our success comes from our energetic and
-          young team. We believe in the fresh ideas and energy that young people bring.
-          Our team isn't just a group of workers; they're passionate folks who love to
-          explore new ideas and stay on top of fashion trends. Their excitement keeps
-          our brand always fresh and exciting.
+        <p>A Dynamic and Youthful Team: Our success comes from our energetic and young team. We believe in the fresh ideas and energy that young people bring. Our team isn't just a group of workers; they're passionate folks who love to explore new ideas and stay on top of fashion trends. Their excitement keeps our brand always fresh and exciting. 
 
         </p>
-        <p>Founder's Expertise: With a founder who boasts an impressive 25 years of
-          experience in the fashion industry, our brand benefits from a wealth of
-          knowledge and insight. Our founder's extensive journey in the world of fashion
-          has equipped us with a deep understanding of the industry's nuances, trends,
-          and intricacies. This expertise is the foundation upon which our brand is built,
-          enabling us to make informed decisions, curate exceptional collections, and
-          provide our customers with fashion that is not only stylish but also imbued
-          with a rich heritage of expertise.
+        <p>Founder's Expertise: With a founder who boasts an impressive 25 years of experience in the fashion industry, our brand benefits from a wealth of knowledge and insight. Our founder's extensive journey in the world of fashion has equipped us with a deep understanding of the industry's nuances, trends, and intricacies. This expertise is the foundation upon which our brand is built, enabling us to make informed decisions, curate exceptional collections, and provide our customers with fashion that is not only stylish but also imbued with a rich heritage of expertise.
+
 
         </p>
       </div>
@@ -141,25 +105,26 @@
         </div>
       </div>
 
-      <p>  As Poshida Fashions LLP began to flourish, the co-founders, Usha Singh, Rekha Rohilla, and  Pawan Rohilla, found themselves immersed in the dynamic world of fashion and entrepreneurship.  Usha Singh, armed with her BBA degree, skillfully managed the business operations, ensuring that Poshida Fashions LLP not only created beautiful clothing but also ran efficiently as a brand.</p>
+      <p> Embarking on a transformative journey, Usha Singh navigated challenges, celebrated successes, and shaped <b> Poshida Fashions LLP </b> into a household name. With an unwavering commitment to quality, creativity, and customer satisfaction, she propelled the brand to new heights, turning  <b> Poshida Fashions LLP </b> into a symbol of elegance and style.</p>
 
 
 
-      <p> Rekha Rohilla, with her BA degree and innate creativity, played a pivotal role in shaping Poshida Fashions LLP's unique aesthetic. Her passion for fashion and design fueled the brand's innovation, introducing styles that resonated with customers looking for something beyond the ordinary.</p>
+      <p> In the vibrant tapestry of <b> Poshida Fashions LLP's </b> evolution, <b> Usha Singh </b> stood as the steadfast pillar of success, proving that a blend of business acumen, creativity, and experience could craft a fashion empire that not only adorned bodies but also celebrated the spirit of individuality.</p>
 
 
-      <p> Pawan Rohilla, the seasoned fashion designer, brought his wealth of experience to the table. With 18 years in fashion design, apparel manufacturing, and development,  Pawan Rohilla's expertise ensured that every Poshida Fashions LLP creation was a masterpiece. His designs seamlessly blended tradition and modernity, captivating the hearts of fashion enthusiasts.</p>
+      <p> While our size may have changed over the years, our core belief remains constant – the customer is at the center of everything we do. As a closely-knit family, we have focused on the key elements of fashion production – design, manufacture, distribution, and retail. With pillars of flexibility, digital integration, and sustainability, we have brought our customers closer than ever to the products they desire, all at affordable prices
+</p>
 
-      <p> Together, Usha Singh,Rekha Rohilla, and  Pawan Rohilla navigated challenges, celebrated successes, and transformed Poshida Fashions LLP into a household name. Their commitment to quality, creativity, and customer satisfaction propelled the brand to new heights, making Poshida Fashions LLP a symbol of elegance and style.</p>
+      <p> <b> Poshida Fashions LLP </b> is not just a brand; it's a testament to perseverance, innovation, and the enduring connection between a fashion label and its patrons. With simplicity and style at our core, we continue to write new chapters in our story, always putting our customers first.</p>
 
 
-      <p>In the grand tapestry of Poshida Fashions LLP's journey,Usha Singh, Rekha Rohilla, and  Pawan Rohilla stood as the pillars of success, showcasing that a blend of business acumen, creativity, and experience could create a fashion empire that not only adorned bodies but also celebrated the spirit of individuality.</p>
-     
-      <p style="color: black;"> <b>Established in Jaipur, this ready-to-wear brand has come to be synonymous with versatile yet powerful casual-wear.</b> </p>
+      <!-- <p>In the grand tapestry of Poshida Fashions LLP's journey,Usha Singh, Rekha Rohilla, and  Pawan Rohilla stood as the pillars of success, showcasing that a blend of business acumen, creativity, and experience could create a fashion empire that not only adorned bodies but also celebrated the spirit of individuality.</p>
+      -->
+      <!-- <p style="color: black;"> <b>Established in Jaipur, this ready-to-wear brand has come to be synonymous with versatile yet powerful casual-wear.</b> </p>
     <p style="color: black;"> <b>We have been constantly evolving and expanding the product line and ruling the hearts of loyal customers since 2019.</b> </p>
     <p>Over the years, our size may have changed, but one underlying idea has stayed the same – the customer is at the centre of everything we do.</p>
     <p>By working closely together as a family, WE focused on the key elements of fashion production –design, manufacture, distribution and retail– with three pillars -flexibility, digital integration and sustainability- we brought our customers
-      closer than ever to the products they wanted at affordable prices.</p> 
+      closer than ever to the products they wanted at affordable prices.</p>  -->
     </div>
   </div>
 </div>
