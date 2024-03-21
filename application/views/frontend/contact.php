80d9af10124f2ae1efa21103898bb3187cc43285
Deepak choudhary <deepakkalirawna251@gmail.com>
2024-03-21 10:43:44 +0530

    map update

--- application/views/frontend/contact.php
+++ application/views/frontend/contact.php
@@ -18,7 +18,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="add-ma pb-100">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.055048251228!2d75.77483437443958!3d26.838201376691636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db5918ededfb9%3A0xbaa9c10236eec525!2sUmmed%20Mills%20India%20(Reg)!5e0!3m2!1sen!2sin!4v1707723906243!5m2!1sen!2sin" height="585" style="border:0;width:100%;" allowfullscreen></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.05727021786!2d75.77740639999999!3d26.8381307!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db5911266f1cf%3A0x1189ecb28f545112!2sPoshida%20clothing%20store%20-%20mansrover!5e0!3m2!1sen!2sin!4v1710997495589!5m2!1sen!2sin"height="585" style="border:0;width:100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 
                     </div>
